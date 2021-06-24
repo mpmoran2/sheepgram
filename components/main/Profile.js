d371fd3e4ca03629f69c840d5fb17b9a9de8bd49
mpmoran2 <kamokumoran@gmail.com>
2021-06-24 14:24:52 -0700

    Bottom Tab Pages created w/icons

---
+++ components/main/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+export default function Profile() {
+    return (
+        <View>
+            <Text>This is you!</Text>
+        </View>
+    )
+}
